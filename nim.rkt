98949d12485ff88fb1f8db6290a421d7134844c5
N Harvey <nthnhrvy@gmail.com>
2013-02-15 15:48:01 -0700

    now actually prints the board semi-properly

--- nim.rkt
+++ nim.rkt
@@ -1,6 +1,10 @@
 (define (pb brd) ;function to print the board
-  [if (null? brd)()((display (car brd))
-  (newline)
-  (pb (cdr brd)))])
+  [if (null? brd)(display " ")(begin 
+    (display (car brd))
+    (newline)
+    (pb (cdr brd)))])
 (define (nim board players)
-  (pb board))
+  (pb board)
+  (play players)
+  (display (car players))
+)
