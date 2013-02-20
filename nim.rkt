3815c9c3684130154b681ad304dc9f3bc181ad95
N Harvey <nthnhrvy@gmail.com>
2013-02-19 21:22:22 -0700

    now switches players

--- nim.rkt
+++ nim.rkt
@@ -11,15 +11,17 @@
     (pb (cdr brd) (+ row 1)))])
 (define (play board players who)
   (pb board 0);print the board
-  (play (rem board (p-row (list-ref players who)) (p-num(list-ref players who))) players who);remove the one they chose
+  (display "Player")(display who)(display "'s turn.")(newline)
+  (play (rem board (p-row (list-ref players who)) (p-num(list-ref players who))) players (bitwise-xor who 1))
+  ;remove the one they chose, then switch players (via the xor).
 )
-(define (p-row player)(cond
-   [(equal? player 'human)(display "Enter a Row:")(read)]
+(define (p-row player)(cond   ; pick row function
+   [(equal? player 'human)(display "Enter a Row: ")(read)]
    [(equal? player 'random)()]
    [(equal? player 'smart)()]
    ))
-(define (p-num player)(cond
-   [(equal? player 'human)(display "Enter number to remove:")(read)]
+(define (p-num player)(cond   ; pick number to remove function
+   [(equal? player 'human)(display "Enter number to remove: ")(read)]
    [(equal? player 'random)()]
    [(equal? player 'smart)()]
    ))
@@ -33,5 +35,15 @@
       (rm (remove 'X lst) (- count 1))
   )
 )
+;(map (lambda (x)(bitwise-xor 1 x)) '(7 1 2 3 5))
 
-(nim '((X X) (X) (X X) (X)) '(human human)); FOR TESTING ONLY
+(nim '((X X X) (X X X X X X) (X X) (X)) '(human human)); FOR TESTING ONLY
+
+#| XOR them all together with apply
+if the result is not zero, then XOR with each element, to build another list. 
+Subtract each XOR from each original element 
+|#
+
+;player who receives an empty board loses; check right before you prompt.
+
+;loren.blaney@gmail.com  email source when done.
