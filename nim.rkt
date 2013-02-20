537a5ce8e50af835c5e371c7e2e00a276efd4026
N Harvey <nthnhrvy@gmail.com>
2013-02-19 22:13:03 -0700

    game ends with human players, correct winner

--- nim.rkt
+++ nim.rkt
@@ -1,5 +1,6 @@
+(require racket/list); for some functions such as flatten.
+
 (define (nim board players)
-  (if (null? board)(error "The first argument should be a non-empty board"));check board
   (if (not(eq?(length players)2)) (error "There should be exactly 2 players."))
   (play board players 0);"loop" to keep doing turns, starting with player 0.
 )
@@ -11,19 +12,24 @@
     (pb (cdr brd) (+ row 1)))])
 (define (play board players who)
   (pb board 0);print the board
-  (display "Player")(display who)(display "'s turn.")(newline)
-  (play (rem board (p-row (list-ref players who)) (p-num(list-ref players who))) players (bitwise-xor who 1))
+  (if (null? (flatten board)) 
+      (begin (display "Player ")(display (bitwise-xor who 1))(display " Won!")
+             (bitwise-xor who 1)) ;if we have empty board, the other player won.
+  (begin (display "Player ")(display who)(display "'s turn.")(newline)     
+  (play (rem board (p-row (list-ref players who) board) (p-num (list-ref players who) board)) players (bitwise-xor who 1))
   ;remove the one they chose, then switch players (via the xor).
-)
-(define (p-row player)(cond   ; pick row function
+)))
+(define (p-row player board)(cond   ; pick row function
    [(equal? player 'human)(display "Enter a Row: ")(read)]
    [(equal? player 'random)()]
    [(equal? player 'smart)()]
+   [error "bad player type"]
    ))
-(define (p-num player)(cond   ; pick number to remove function
+(define (p-num player board)(cond   ; pick number to remove function
    [(equal? player 'human)(display "Enter number to remove: ")(read)]
    [(equal? player 'random)()]
    [(equal? player 'smart)()]
+   [error "bad player type"]
    ))
 (define (rem lst row count); Removes count elements from the row'th sub-list of the list lst.
   (if(eqv? row 0)(cons (rm (car lst) count) (cdr lst))
