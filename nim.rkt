6524e9de914211d323a58b916f00da7fe79a74a0
N Harvey <nthnhrvy@gmail.com>
2013-02-20 12:36:06 -0700

    Need to fix remove entire list problem...

--- nim.rkt
+++ nim.rkt
@@ -16,26 +16,26 @@
       (begin (display "Player ")(display (bitwise-xor who 1))(display " Won!")
              (bitwise-xor who 1)) ;if we have empty board, the other player won.
   (begin (display "Player ")(display who)(display "'s turn.")(newline)     
-  (play (rem board (p-row (list-ref players who) board) (p-num (list-ref players who) board)) players (bitwise-xor who 1))
+  (play (rem board (list(p-row (list-ref players who) board) (p-num (list-ref players who) board))) players (bitwise-xor who 1))
   ;remove the one they chose, then switch players (via the xor).
 )))
 (define (p-row player board)(cond   ; pick row function
    [(equal? player 'human)(display "Enter a Row: ")(read)]
-   [(equal? player 'random)()]
+   [(equal? player 'random)(random (length board))]
    [(equal? player 'smart)()]
    [error "bad player type"]
    ))
 (define (p-num player board)(cond   ; pick number to remove function
    [(equal? player 'human)(display "Enter number to remove: ")(read)]
-   [(equal? player 'random)()]
+   [(equal? player 'random)0]
    [(equal? player 'smart)()]
    [error "bad player type"]
    ))
-(define (rem lst row count); Removes count elements from the row'th sub-list of the list lst.
-  (cond [(eqv? row 0)(cons (rm (car lst) count) (cdr lst))]
-        [(or (< row 0)(> row (length lst)))(display "Bad Row.\n")lst]
-        [(> count (length (list-ref lst row))) (display "Not enough Sticks there.\n")lst]
-        [cons (car lst) (rem (cdr lst) (- row 1) count)]
+(define (rem lst move); Removes the move from the list lst.
+  (cond [(eqv? (car move) 0)(cons (rm (car lst) (cadr move)) (cdr lst))]
+        [(or (< (car move) 0)(> (car move) (length lst)))(display "Bad Row.\n")lst]
+        [(> (cadr move) (length (list-ref lst (car move)))) (display "Not enough Sticks there.\n")lst]
+        [cons (car lst) (rem (cdr lst) (list(- (car move) 1) (cadr move)))]
   )
 )
 (define (rm lst count);takes a flat list and removes count X's
@@ -51,7 +51,5 @@
 if the result is not zero, then XOR with each element, to build another list. 
 Subtract each XOR from each original element 
 |#
-
-;player who receives an empty board loses; check right before you prompt.
 
 ;loren.blaney@gmail.com  email source when done.
